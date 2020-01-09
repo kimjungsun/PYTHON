b9aa6ab99b45cac19598c5f1a367e9af505a16eb
Megan Kim <33720329+kimjungsun@users.noreply.github.com>
2020-01-09 16:38:45 +0900

    Update linkedlist_init.py

--- linkedlist_init.py
+++ linkedlist_init.py
@@ -1,1 +1,34 @@
+#  Node Class 
 class node : 
+  def __init__(self,val) :
+    self.data = val
+    self. next = None
+#  Linked List Class
+class Linked_List:
+  def __init__(self):
+    self.head = None
+    
+   def insert(self,val):
+    if self.head == None :
+      self.head = node(val)
+    else :
+      new_node = node(val)
+      temp = self.head
+      while(temp.next):
+        temp = temp.next
+       temp.next = new_node
+      
+def createList(arr,n) :
+  lis = Linked_List()
+  for i in range(n):
+    lis.insert(arr[i])
+    return lis.head 
+
+if __name__ =='__main__':
+  t =int(input())
+  for i in range(t):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    head = createList(arr,n)
+   
+  
